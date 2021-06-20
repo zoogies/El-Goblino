9c8f2c161c8f208fd604403bbee57b554b7349a5
Ryan Zmuda <ryanzmuda@gmail.com>
2021-06-20 19:20:36 -0400

    :smile: finessed it to work

--- src/main.py
+++ src/main.py
@@ -9,16 +9,18 @@ import requests
 from discord.ext import commands, tasks
 
 #verbose mode
-v = True
+v = False
 
 #static variables
 client = discord.Client()
+#username = 'xqcow'
 username = 'xqcow'
 twitchAPIEndpoint = "https://api.twitch.tv/helix/streams?user_login=" + username
 twitchKeyCooldown = 24 #in hours
-checkLiveCooldown = 20 #in seconds
+checkStatusCooldown = 20
+cooldownWhenOffline = 20 #in seconds
+cooldownWhenLive = 60
 isLive = False
-channelName = 'live-notifs'
 
 #declare hidden variables
 discordToken = 'loaded from tokens.csv'
@@ -43,13 +45,15 @@ try:
     discordToken = tokens[0]
     twitchSecret = tokens[1]
     twitchAPIheaders['Client-ID'] = tokens[2]
-
+    channel = (int(tokens[3]))
     #Debug shit, TODO remove
     if v:
         print(tokens)
         print(discordToken)
         print(twitchSecret)
         print(twitchAPIheaders['Client-ID'])
+        print(int(tokens[3]))
+        print(channel)
 
 except Exception as e:
     print('El Goblino couldnt find the tokens.csv file, make sure it exists')
@@ -82,17 +86,15 @@ def checkLiveStatus():
             print(requestReturn)
         print('El Goblino just got the live status, i wonder if my juicer is live')
 
-        print(requestReturn)
         with open('data.json', 'w') as f:
             json.dump(requestReturn, f)
-        
-        data = requestReturn['data'][0]
+        try:
+            data = requestReturn['data'][0]
+        except:
+            return #TODO stream data returns whats in the json rn when not live.
         if v:
             print(data)
         return data
-
-        #TODO this is where i left off, take the data from this object and load it into a discord message, also add this function to the bindable flow check every like 10 seconds if he went live
-        #https://python.plainenglish.io/send-an-embed-with-a-discord-bot-in-python-61d34c711046
 
     except Exception as e:
         print('El Goblino failed getting the live status, i enjoyed my stay xqcL')
@@ -100,48 +102,52 @@ def checkLiveStatus():
             print(e)
         return False
 
-def embed(streamData,channel):
+async def embed(streamData,channel):
+    channel = client.get_channel(channel)
 
     title = streamData['title']
     url = 'https://www.twitch.tv/xqcow'
-    description = 'xQcOW has gone live!'
+    description = 'xQcOW is now live!'
+    image = "https://pbs.twimg.com/profile_images/1188911868863221772/fpcyKuIW_400x400.jpg"
+    game = streamData["game_name"]
 
     embed=discord.Embed(title=title, url=url, description=description, color=0x20fc03)
-    if channel:
-        channel.send(embed)
-    else:
-        print('El goblino couldnt send a message to the channel because it hasnt been set')
+    embed.set_thumbnail(url=image)
+    embed.set_author(name="xQcOW has gone live!", url="https://www.twitch.tv/xqcow", icon_url="https://pbs.twimg.com/profile_images/1188911868863221772/fpcyKuIW_400x400.jpg")
+    embed.set_thumbnail(url="https://i.popdog.com/0777a661-6332-11ea-ae2b-0a58a9feac2a")
+    embed.add_field(name="Playing", value=game, inline=True)
+    embed.set_footer(text="Get the juice baby 😈")
+    await channel.send(embed=embed)
 
-@tasks.loop(seconds=checkLiveCooldown)
-async def mainLoop(isLive,channel):
+@tasks.loop(seconds=checkStatusCooldown)
+async def mainLoop(channel):
     #get the steam data and update our state
     streamData = checkLiveStatus()
-    if streamData:
+    global isLive
+
+    if streamData != None:
         #only update our variable for live if he was previously offline
         if streamData['type'] == 'live' and isLive == False:
             isLive = True
+            checkLiveCooldown = cooldownWhenLive
             #send an embed to let me know that hes live
-            embed(streamData,channel)
-        else:
-            isLive = False
+            await embed(streamData,channel)
     else:
-        print('Something went wrong when El Goblino tried to recieve the stream data :(')
+        print('Juicer is offline :(')
+        isLive = False
+        checkLiveCooldown = cooldownWhenOffline
 
 @client.event
 async def on_ready():
     print('{0.user} is now online.'.format(client))
     getNewAuthTwitchAPI.start()
-    mainLoop.start(isLive,channel)
-
-def setChannel(channel):
-    channel = channel
+    mainLoop.start(channel)
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('GOBLIN INITIATE'):
-        setChannel(message.chanel)
         await message.channel.send('Goblin is now craling around in "'+str(message.channel)+'"')
 
 client.run(discordToken)
